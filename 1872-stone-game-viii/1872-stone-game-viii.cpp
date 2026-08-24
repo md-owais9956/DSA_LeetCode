c3d1f635f4af5a98f6fd276f798ff75b1f256291
MOHAMMAD OWAIS <md.owais111234@gmail.com>
2026-08-24 21:43:34 +0530

    Time: 114 ms (35.29%), Space: 87.3 MB (84.19%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -3,20 +3,17 @@ public:
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
 
-        vector<int> prefix(n);
-        prefix[0] = stones[0];
-
+       
         for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + stones[i];
+            stones[i] += stones[i - 1];
         }
 
-      
-        int dp = prefix[n - 1];
+        int ans = stones[n - 1];
 
         for (int i = n - 2; i >= 1; i--) {
-            dp = max(dp, prefix[i] - dp);
+            ans = max(ans, stones[i] - ans);
         }
 
-        return dp;
+        return ans;
     }
 };
