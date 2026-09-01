5f5594be7de33ffb63ccc949b29018d397c51430
MOHAMMAD OWAIS <md.owais111234@gmail.com>
2026-09-01 19:43:59 +0530

    Time: 219 ms (79.59%), Space: 157.3 MB (69.39%) - LeetHub

---
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -0,0 +1,114 @@
+class Solution {
+public:
+    int minMoves(vector<string>& classroom, int energy) {
+        int m = classroom.size();
+        int n = classroom[0].size();
+
+        int sr, sc;
+        vector<pair<int, int>> litter;
+
+        // Find S and all L cells
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (classroom[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                } 
+                else if (classroom[i][j] == 'L') {
+                    litter.push_back({i, j});
+                }
+            }
+        }
+
+        int k = litter.size();
+
+        // Map each litter cell to its bit
+        vector<vector<int>> id(m, vector<int>(n, -1));
+
+        for (int i = 0; i < k; i++) {
+            id[litter[i].first][litter[i].second] = i;
+        }
+
+        int fullMask = (1 << k) - 1;
+
+        // If there is no litter
+        if (k == 0) return 0;
+
+        /*
+            best[r][c][mask] = maximum energy with which
+            we have reached (r,c) having collected mask.
+        */
+        vector<vector<vector<int>>> best(
+            m,
+            vector<vector<int>>(n, vector<int>(1 << k, -1))
+        );
+
+        struct State {
+            int r, c, mask, en, dist;
+        };
+
+        queue<State> q;
+
+        best[sr][sc][0] = energy;
+        q.push({sr, sc, 0, energy, 0});
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+            auto cur = q.front();
+            q.pop();
+
+            int r = cur.r;
+            int c = cur.c;
+            int mask = cur.mask;
+            int en = cur.en;
+            int dist = cur.dist;
+
+            if (mask == fullMask)
+                return dist;
+
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                // Outside grid
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+                    continue;
+
+                // Obstacle
+                if (classroom[nr][nc] == 'X')
+                    continue;
+
+                // Need one energy to make a move
+                if (en == 0)
+                    continue;
+
+                int nen = en - 1;
+                int nmask = mask;
+
+                // Collect litter
+                if (classroom[nr][nc] == 'L') {
+                    nmask |= (1 << id[nr][nc]);
+                }
+
+                // Reset energy on R
+                if (classroom[nr][nc] == 'R') {
+                    nen = energy;
+                }
+
+                // If this state is not better, skip it
+                if (best[nr][nc][nmask] >= nen)
+                    continue;
+
+                best[nr][nc][nmask] = nen;
+
+                q.push({
+                    nr, nc, nmask, nen, dist + 1
+                });
+            }
+        }
+
+        return -1;
+    }
+};
